5e8002eaa9271b8005a97767253aa717521bdb25
Aoi <19519928+AoiYamada@users.noreply.github.com>
2020-10-01 23:31:44 +0800

    feat: auth validation draft

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserModule],
+  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
+import { UserEntity } from 'src/user/user.entity';
+import { UserRepository } from 'src/user/user.repository';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, UserService, UserRepository],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    service = await module.resolve(AuthService);
+    userService = await module.resolve(UserService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('validateUser should return user if password matches', async () => {
+    const username = 'tester';
+    const password = 'MyPassword';
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new UserEntity();
+
+    user.username = username;
+    user.password = hashedPassword;
+
+    const userServiceSpy = {
+      getByUsername: jest
+        .spyOn(userService, 'getByUsername')
+        .mockImplementation(async () => user),
+    };
+
+    const userEntitySpy = {
+      verifyPassword: jest.spyOn(user, 'verifyPassword'),
+    };
+
+    const result = await service.validateUser(username, password);
+
+    expect(userServiceSpy.getByUsername).toHaveBeenLastCalledWith(username);
+    expect(userEntitySpy.verifyPassword).toHaveBeenLastCalledWith(password);
+    expect(result).toEqual(user);
+  });
+
+  it('validateUser should return null if password does not match', async () => {
+    const username = 'tester';
+    const password = 'MyPassword';
+
+    const user = new UserEntity();
+
+    user.username = username;
+    user.password = password;
+
+    const userServiceSpy = {
+      getByUsername: jest
+        .spyOn(userService, 'getByUsername')
+        .mockImplementation(async () => user),
+    };
+
+    const userEntitySpy = {
+      verifyPassword: jest.spyOn(user, 'verifyPassword'),
+    };
+
+    const result = await service.validateUser(username, password);
+
+    expect(userServiceSpy.getByUsername).toHaveBeenLastCalledWith(username);
+    expect(userEntitySpy.verifyPassword).toHaveBeenLastCalledWith(password);
+    expect(result).toEqual(null);
+  });
+
+  it('validateUser should return null if user does not exist', async () => {
+    const username = 'tester';
+    const password = 'MyPassword';
+
+    const userServiceSpy = {
+      getByUsername: jest
+        .spyOn(userService, 'getByUsername')
+        .mockImplementation(async () => undefined),
+    };
+
+    const result = await service.validateUser(username, password);
+
+    expect(userServiceSpy.getByUsername).toHaveBeenLastCalledWith(username);
+    expect(result).toEqual(null);
   });
 });
